97ddd538882f4e00aa101a9fc666607c808c6b52
Glerup <k3dd3@hotmail.dk>
2026-09-06 01:56:46 +0200

    Add cache-busted safe AirPlay asset injector

---
+++ safe_airplay_assets.py
@@ -0,0 +1,29 @@
+from flask import Response, request
+
+
+def _inject_safe_airplay_assets(response: Response) -> Response:
+    try:
+        if request.path != "/" or response.status_code != 200 or response.mimetype != "text/html":
+            return response
+        text = response.get_data(as_text=True)
+        marker = "fjordlens-safe-airplay-assets"
+        if marker in text:
+            return response
+        scripts = (
+            f'<script id="{marker}" src="/static/cast_airplay_safe.js?v=20260906-2"></script>\n'
+            '<script src="/static/airplay_hls_safe.js?v=20260906-2"></script>\n'
+        )
+        if "</body>" in text:
+            text = text.replace("</body>", scripts + "</body>", 1)
+        response.set_data(text)
+        response.headers.pop("Content-Length", None)
+    except Exception:
+        pass
+    return response
+
+
+def init_safe_airplay_assets(flask_app) -> None:
+    if flask_app.extensions.get("safe_airplay_assets_registered"):
+        return
+    flask_app.after_request(_inject_safe_airplay_assets)
+    flask_app.extensions["safe_airplay_assets_registered"] = True
